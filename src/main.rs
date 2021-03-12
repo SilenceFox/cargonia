efa8b11982b609a292574084d7a610864d462534
SilenceFox <cristianplaybr@gmail.com>
2021-03-12 00:48:15 -0300

    joao preguiçoso

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use bevy::prelude::*;
+
+
+
+
+fn hello_world() {
+    println!("hello world!");
+}
+
+struct Person;
+struct Name(String);
+
+fn add_people(commands: &mut Commands) {
+    commands
+        .spawn((Person, Name("Elaina Proctor".to_string())))
+        .spawn((Person, Name("Renzo Hume".to_string())))
+        .spawn((Person, Name("Zayna Nieves".to_string())));
+}
+
 fn main() {
-    println!("Hello, world!");
+    App::build()
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(add_people.system())
+        #.add_system(hello_world.system())
+        .add_system(greet_people.system())
+        .run();
+}
+
+fn greet_people(query: Query<&Name, With<Person>>) {
+    for name in query.iter() {
+        println!("hello {}!", name.0);
+    }
 }
